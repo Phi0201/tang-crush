ff2a20554f6a45cd079e0c15cc5923a95124fce3
Phi0201 <88325102+Phi0201@users.noreply.github.com>
2021-08-02 05:19:10 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Phương Vy à!',
+    introDesc: `Có món quà nhỏ tặng cho em nè`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Phương Vy yêu anh nhiều lắm hongg',
+    desc: 'Hong trả lời là yêu nhiều nhaaa ',
+    btnYes: 'Yêuuu Nhiềuu <33',
+    btnNo: 'Không Yêu Nhiềuu :vv',
+    question:'Sao lại yêu anh nhỉ =))))',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Hong yêu nhiều cũng phải yeeuuu =))))',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay mơ về anh thật nhiều nhá.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/song.phi.735' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
